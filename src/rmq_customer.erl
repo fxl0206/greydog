0ea6c5f2796728f4e02dbf92e82439ed5bf92390
fxl0206 <fxl0206@github.com>
2015-05-07 18:07:08 +0800

    modified:   src/rmq_customer.erl

--- src/rmq_customer.erl
+++ src/rmq_customer.erl
@@ -1,7 +1,7 @@
 -module(rmq_customer).
 -include_lib("amqp_client/include/amqp_client.hrl").
 -export([main/0]).
--export([save_db/1]).
+-export([save_db/3]).
 main() ->
     {ok, Connection} =
         amqp_connection:start(#amqp_params_network{username = <<"admin">>,password = <<"admin">>,host = "123.56.90.92"}),
@@ -11,7 +11,7 @@ main() ->
     io:format(" [*] Waiting for messages. To exit press CTRL+C~n"),
     amqp_channel:call(Channel, #'basic.qos'{prefetch_count = 1}),
     amqp_channel:subscribe(Channel, #'basic.consume'{queue = <<"wx_msg">>,
-                                                     no_ack = true}, self()),
+                                                     no_ack = false}, self()),
     receive
         #'basic.consume_ok'{} -> ok
     end,
@@ -20,15 +20,16 @@ main() ->
 
 loop(Channel) ->
     receive
-        {#'basic.deliver'{}, #amqp_msg{payload = Body}} ->
-	spawn(?MODULE,save_db,[Body]), 
+        {#'basic.deliver'{delivery_tag = Tag}, #amqp_msg{payload = Body}} ->
+	spawn(?MODULE,save_db,[Body,Channel,Tag]), 
 	io:format(" [x] Received ~p~n", [Body]),
             loop(Channel)
     end.
-save_db(Body)->
+save_db(Body,Channel,Tag)->
 	{FromUserName,ToUserName,Content}=xml_parse(binary_to_list(Body)),
         Sql="insert into ts_wx_msg(msgid,type,content,fuser,tuser,create_time) values('1','text','"++Content++"','"++FromUserName++"','"++ToUserName++"',now())",
         mysql:fetch(conn,unicode:characters_to_binary(Sql)),
+        amqp_channel:call(Channel,#'basic.ack'{delivery_tag = Tag}),
 	ok.
 %weixin xml parse
 xml_parse(Xml)->
